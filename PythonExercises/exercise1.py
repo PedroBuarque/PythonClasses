697e2922d7e5cc054e46a29a2aac17f7fb95cc93
PedroBuarque <pedroh.buarque@gmail.com>
2023-12-28 15:44:35 -0300

    Add concluded exercise

---
+++ PythonExercises/exercise1.py
@@ -0,0 +1,12 @@
+name = 'Pedro Henrique'
+surname = 'Bezerra Buarque de Paula'
+age = 25
+birth_year = 2023 - age
+of_legal_age = age >= 18
+height = 1.76
+
+print('Name:', name)
+print('Surname:', surname)
+print('Birth Year:', birth_year)
+print('On legal age?', of_legal_age)
+print('Height:', height)
