3afa4b5e81aab9adcfda2137728a8d1b4f28f2b6
PedroBuarque <pedroh.buarque@gmail.com>
2024-01-08 20:53:30 -0300

    Add class about basic operators

---
+++ PythonClasses/class8.py
@@ -0,0 +1,22 @@
+#basic operators
+
+sum = 10 + 10
+print('Sum 10 + 10:', sum)
+
+subtraction = 10 - 9
+print('Subtraction 10 - 9:', subtraction)
+
+multiplication = 10 * 10
+print('Multiplication 10 * 10:', multiplication)
+
+division = 10 / 10 #always returns a float
+print('Division 10 / 10:', division)
+
+whole_division = 10 // 2
+print('Whole Division 10 % 2:', whole_division)
+
+exponentiation = 2 ** 10
+print('Exponentiation 2 ** 10:', exponentiation)
+
+module = 55 % 2 #returns the rest of the division
+print('Module 55 % 2:', module)
