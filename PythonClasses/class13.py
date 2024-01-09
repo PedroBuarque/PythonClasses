a34542ad066dc02b710a5bdd441041f6a440e322
PedroBuarque <pedroh.buarque@gmail.com>
2024-01-09 20:20:16 -0300

    Add class about the basics of user inputs

---
+++ PythonClasses/class13.py
@@ -0,0 +1,9 @@
+#working with the basic of user inputs
+
+num_1 = input('Type a number: ')
+num_2 = input('Type another number: ')
+
+int_num_1 = int(num_1)
+int_num_2 = int(num_2)
+
+print(f'The sum of the numbers you typed is: {int_num_1 + int_num_2}')
