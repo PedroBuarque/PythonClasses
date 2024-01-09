182e89149df57d7d57cff02e5c2543aa3b7e0cd1
PedroBuarque <pedroh.buarque@gmail.com>
2024-01-08 21:22:18 -0300

    Add exercise calculating my IMC using basic operators and string manipulation

---
+++ PythonExercises/exercise2.py
@@ -0,0 +1,8 @@
+#Calculating my CMI
+
+name = 'Pedro' + ' ' + 'Buarque'
+height = 1.75
+weight = 76
+cmi = weight / (height ** 2)
+
+print(name, 'has the height of', height, 'meters, his weight is', weight, 'kilograms, and his CMI is:', cmi)
