216daecaf4564ac41530ec861ac8c522a099d435
PedroBuarque <pedroh.buarque@gmail.com>
2024-01-08 21:01:11 -0300

    Add class about basic concatanation using strings and operators

---
+++ PythonClasses/class9.py
@@ -0,0 +1,7 @@
+concatenation = 'Pedro' + ' ' + 'Buarque'
+print("The concatanation of Pedro + ' ' + Buarque:", concatenation)
+
+a_ten_times = 'A' * 10
+three_times_pedro = 3 * 'Pedro'
+print('A ten times using concatanation:', a_ten_times)
+print('Three times Pedro using concatation:', three_times_pedro)
