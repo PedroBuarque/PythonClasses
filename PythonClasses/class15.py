7dce5cb7199bd8ce11b6fbf05a2f71de4302e80b
PedroBuarque <pedroh.buarque@gmail.com>
2024-01-16 22:54:54 -0300

    Add class about comparison operators

---
+++ PythonClasses/class15.py
@@ -0,0 +1,20 @@
+"""
+comparison operators:
+OP |  What it means | Example (True)
+----------------------------------------
+>  | Greater | 2 > 1 -> True
+>= | Greater or equal | 2 >= 2 -> True
+<  | Minor | 1 < 2 -> True
+<= | Minor or equal | 2 <= 2 -> True
+== | Equal | 'a' == 'a' -> True
+!= | Different | 'a' != 'b' -> True
+----------------------------------------
+"""
+greater = 2 > 1
+greater_or_equal = 2 >= 2
+minor = 1 < 2
+minor_or_equal = 2 <= 2
+equal = 'a' == 'a'
+different = 'a' != 'b'
+
+print(different)
